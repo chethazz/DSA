7aa92122d3c83ed4ccecefc39b4d557694d6caf8
chethazz <chethaskp10@gmail.com>
2025-12-08 09:11:40 +0530

    linear search

--- Algorithms/1linearSearch.js
+++ Algorithms/1linearSearch.js
@@ -1,1 +1,12 @@
 // Big O(n)
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) return i;
+    }
+
+    return -1;
+}
+
+console.log(linearSearch(arr, 4));
