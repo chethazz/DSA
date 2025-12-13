e9363f8b2e605af1ef96684dd0d3c7cce6b44e78
chethazz <chethaskp10@gmail.com>
2025-12-13 13:18:17 +0530

    pascals triangle

--- Patterns/patterns.js
+++ Patterns/patterns.js
@@ -95,3 +95,18 @@ function leftLeaningRhombus(rows) {
         console.log(row);
     }
 }
+
+function pascalsTriangle(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let row = "";
+        for (let j = 0; j < rows - i; j++) {
+            row += " ";
+        }
+        let num = 1;
+        for (let k = 1; k <= i; k++) {
+            row += num + " ";
+            num = num * (i - k) / k;
+        }
+        console.log(row);
+    }
+}
