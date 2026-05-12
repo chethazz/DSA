bff6b098fb21b0b084f8b2383a7e05c0a29f7989
chethazz <chethaskp10@gmail.com>
2026-05-12 16:31:48 +0530

    begin

---
+++ nest-learning/src/cats/cats.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): string {
+    return `This creates a cat with ${createCatDto.name}, ${createCatDto.age}, ${createCatDto.breed}`;
+  }
+
+  @Get()
+  findAll(@Query('age') age: number, @Query('breed') breed: string): string {
+    return `This action returns all cats filtered by age: ${age} and breed: ${breed}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This action deletes a #${id} cat`;
+  }
+}
