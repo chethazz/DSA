0e38b0cd3aff41b74549c85ef8f35da58649266a
chethazz <chethaskp10@gmail.com>
2026-05-14 12:33:27 +0530

    gloabl filter with dependency injection

--- nest-learning/src/app.module.ts
+++ nest-learning/src/app.module.ts
@@ -1,20 +1,29 @@
 import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
+    MiddlewareConsumer,
+    Module,
+    NestModule,
+    RequestMethod,
 } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
+import { HttpExceptionFilter } from './cats/http-exception.filter';
 import { logger } from './middleware/logger.(functional).middleware';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Dependency Injection - global
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- nest-learning/src/main.ts
+++ nest-learning/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+
+  // Global scoped filter. It can't inject dependencies since it's done outside
+  // the context of any module. To fix this, we can register a global scoped filter
+  // directly from any module. (See app.module.ts -> Providers)
+
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
