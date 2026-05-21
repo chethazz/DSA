2278ddcc734c31bfb92d80ef50e369ff741b89be
chethazz <chethaskp10@gmail.com>
2026-05-21 10:27:36 +0530

    extend autogenerated methods(register, registerasync)

--- nest-fundamentals/src/config/config.module-definition.ts
+++ nest-fundamentals/src/config/config.module-definition.ts
@@ -1,14 +1,18 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common';
 import { ConfigModuleOptions } from './interfaces';
 
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<ConfigModuleOptions>()
-    // .setClassMethodName('forRoot')
-    .setExtras({ isGlobal: true }, (definition, extras) => ({
-      ...definition,
-      global: extras.isGlobal,
-    }))
-    .build();
+export const {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN,
+  OPTIONS_TYPE,
+  ASYNC_OPTIONS_TYPE,
+} = new ConfigurableModuleBuilder<ConfigModuleOptions>()
+  // .setClassMethodName('forRoot')
+  .setExtras({ isGlobal: true }, (definition, extras) => ({
+    ...definition,
+    global: extras.isGlobal,
+  }))
+  .build();
 
 // setClassMethodName: Constructor will instruct ConfigurableModuleBuilder to generate a class
 // that exposes forRoot and forRootAsync
--- nest-fundamentals/src/config/config.module.ts
+++ nest-fundamentals/src/config/config.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ConfigurableModuleClass } from './config.module-definition';
+import { DynamicModule, Module } from '@nestjs/common';
+import {
+  ASYNC_OPTIONS_TYPE,
+  ConfigurableModuleClass,
+  OPTIONS_TYPE,
+} from './config.module-definition';
 import { ConfigService } from './config.service';
 
 @Module({
   providers: [ConfigService],
   exports: [ConfigService],
 })
-export class ConfigModule extends ConfigurableModuleClass {}
+export class ConfigModule extends ConfigurableModuleClass {
+  static register(options: typeof OPTIONS_TYPE): DynamicModule {
+    return {
+      ...super.register(options),
+    };
+  }
+
+  static registerAsync(options: typeof ASYNC_OPTIONS_TYPE): DynamicModule {
+    return {
+      ...super.registerAsync(options),
+    };
+  }
+}
