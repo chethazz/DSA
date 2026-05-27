8ede2a326bd1338312e805e91baa1cc81cd0a81b
chethazz <chethaskp10@gmail.com>
2026-05-27 15:33:31 +0530

    message style: req-res

---
+++ microservices/src/math/math.controller.ts
@@ -0,0 +1,28 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { from, Observable } from 'rxjs';
+
+@Controller('math')
+export class MathController {
+  // To create a message handler based on req-res paradigm, use @MessagePattern() decorator.
+  // It should only be within a controller as they serve as the entry point for the app
+
+  // In this snippet, accumulate() handler listens for messages that match {cmd: 'sum'} message pattern. Message handler takes
+  // a single argument, 'data' passed from client. It is an array of  numbers that need to be accumulated.
+  @MessagePattern({ cmd: 'sum' })
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => a + b);
+  }
+
+  // Message handler can respons asynchronously.
+  @MessagePattern({ cmd: 'sumAsync' })
+  async accumulateAsync(data: number[]): Promise<number> {
+    return await Promise.resolve((data || []).reduce((a, b) => a + b));
+  }
+
+  // Can also return an Observable
+  @MessagePattern({ cmd: 'sumObs' })
+  accumulateObs(data: number[]): Observable<number> {
+    return from(data);
+  }
+}
