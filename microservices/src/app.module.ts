f456a41cbe2072472466b2c13d44c80dbaca1a89
chethazz <chethaskp10@gmail.com>
2026-05-28 11:44:44 +0530

    request scoping

--- microservices/src/app.module.ts
+++ microservices/src/app.module.ts
@@ -5,10 +5,12 @@ import { MathController } from './math/math.controller';
 import { ConfigController } from './config/config.controller';
 import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
+import { CatsService } from './cats/cats.service';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [AppController, MathController, ConfigController],
-  providers: [AppService, ConfigService],
+  controllers: [AppController, MathController, ConfigController, CatsController],
+  providers: [AppService, ConfigService, CatsService],
 })
 export class AppModule {}
