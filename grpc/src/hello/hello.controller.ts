3ea4c9be40be27ea232283375637012b9fcff84a
chethazz <chethaskp10@gmail.com>
2026-06-08 12:01:04 +0530

    call stream handler: node.js native approach

--- grpc/src/hello/hello.controller.ts
+++ grpc/src/hello/hello.controller.ts
@@ -1,12 +1,16 @@
 import { Metadata, type ServerDuplexStream } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
-import { GrpcStreamMethod } from '@nestjs/microservices';
+import { GrpcStreamCall, GrpcStreamMethod } from '@nestjs/microservices';
 import { Observable, Subject } from 'rxjs';
+import { HelloResponse } from './hello';
 
 @Controller('hello')
 export class HelloController {
-  @GrpcStreamMethod()
-  bidiHello(
+  // Strategy 1: Full-duplex bidi streaming via RxJS Observables
+  // We can omit the arguments passed to GrpsStreamMethod() and GrpcStreamCall() if method name here was bidiHello
+  // bc nest will infer the names HelloService and bidiHello itself based on the controller name.
+  @GrpcStreamMethod('HelloService', 'BidiHello')
+  bidiHelloRxJS(
     messages: Observable<any>,
     metadata: Metadata,
     call: ServerDuplexStream<any, any>,
@@ -27,5 +31,34 @@ export class HelloController {
     });
 
     return subject.asObservable();
+  }
+
+  // Strategy 2: Full-duplex bidi streaming via native Node.js call streams
+  @GrpcStreamCall('HelloService', 'BidiHello')
+  bidiHelloNative(requestStream: ServerDuplexStream<any, any>) {
+    requestStream.on('data', (message) => {
+      console.log('Native stream server received: ', message);
+
+      requestStream.write({
+        reply: 'Hello from native node stream!',
+      });
+    });
+
+    requestStream.on('end', () => {
+      requestStream.end();
+    });
+  }
+
+  // Can omit arguments here
+  @GrpcStreamCall()
+  lotsOfGreetings(
+    requestStream: ServerDuplexStream<any, any>,
+    callback: (err: unknown, value: HelloResponse) => void,
+  ) {
+    requestStream.on('data', (message) => {
+      console.log(message);
+    });
+
+    requestStream.on('end', () => callback(null, { reply: 'Hello world!' }));
   }
 }
